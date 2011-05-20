2e3683c9cbe85113a40ba4095adc3236a98c4847
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.ru>
2011-05-20 13:48:55 +0400

    Added controller

---
+++ app/controllers/twitter_connect_controller.js
@@ -0,0 +1,65 @@
+var sys = require('sys');
+
+action('connect', function () {
+    req.session.beforeTwitterAuth = req.headers.referer;
+    delete req.session.twitter;
+    consumer().getOAuthRequestToken(gotToken);
+
+    function gotToken (error, oauthToken, oauthTokenSecret, results) {
+        if (error) {
+            flash(error, "Error getting OAuth request token : " + sys.inspect(error));
+            redirectBack();
+        } else {
+            req.session.twitter = {
+                oauthRequestToken: oauthToken,
+                oauthRequestTokenSecret: oauthTokenSecret
+            };
+            redirect(_apiSecureHost + "/oauth/authorize?oauth_token=" + oauthToken);
+        }
+    }
+});
+
+action('callback', function () {
+    consumer().getOAuthAccessToken(
+        req.session.twitter.oauthRequestToken,
+        req.session.twitter.oauthRequestTokenSecret,
+        req.query.oauth_verifier,
+        twitterCallback.bind(this)
+    );
+
+    function twitterCallback (error, oauthAccessToken, oauthAccessTokenSecret, results) {
+        if (error) {
+            send("Error getting OAuth access token : " + sys.inspect(error), 500);
+            return;
+        }
+        consumer().get(
+            _apiHost + "/account/verify_credentials.json",
+            oauthAccessToken,
+            oauthAccessTokenSecret,
+            gotData);
+
+        function gotData (error, data, response) {
+            if (error) {
+                flash(error, "Error getting twitter screen name : " + sys.inspect(error));
+                redirectBack();
+                console.log('gotData:', error);
+            } else {
+                if (typeof data === 'string') {
+                    data = JSON.parse(data);
+                }
+                req.session.twitter = data;
+                req.session.twitter.oauthAccessToken = oauthAccessToken;
+                req.session.twitter.oauthAccessTokenSecret = oauthAccessTokenSecret;
+                app.emit(TWITTER_CONNECT_EVENT, req.session.twitter, req, response);
+                redirectBack();
+            }
+        }
+    }
+});
+
+function redirectBack (f) {
+    var location = req.session && req.session.beforeTwitterAuth || '/';
+    delete req.session.beforeTwitterAuth;
+    redirect(location);
+}
+
