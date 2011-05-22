11fb7c510c9ad2a13475e9add0f8a53600e84d5e
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.ru>
2011-05-22 17:36:43 +0400

    Installation script

---
+++ install.js
@@ -0,0 +1,22 @@
+var config = [
+    'development:',
+    '  url: "http://1.lvh.me"',
+    '  key: key',
+    '  secret: secret',
+    '  connectPath: /twitter_connect',
+    '  callbackPath: /twitter_callback',
+    'production:',
+    '  url: "http://example.com"',
+    '  key: key',
+    '  secret: secret',
+    '  connectPath: /twitter_connect',
+    '  callbackPath: /twitter_callback'
+].join('\n');
+
+var fs = require('fs');
+var path = require('path');
+
+if (path.existsSync(app.root + '/config') && !path.existsSync(app.root + '/config/twitter.yml')) {
+    fs.writeFileSync(app.root + '/config/twitter.yml', config);
+}
+
